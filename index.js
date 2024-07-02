c9326660bd751b02cdf06c3556fbe85599eab6bd
limoh653 <limohesbon7@gmail.com>
2024-07-02 19:57:04 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+function mondayWork(){
+    return mondayWork;
+  }
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+  }
+function wrapAdjective(){
+    return wrapAdjective()
+}
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "a hard worker") {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+
+function wrapAdjective(wrapper = "||") {
+    return function(adjective = "a dedicated programmer") {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}// code your solution here
